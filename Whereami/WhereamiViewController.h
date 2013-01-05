d647f447fe903993cef34b9c2ed1ebad93ed9777
Sanchit Bahal <sanchit.bahal@live.com>
2013-01-05 14:28:02 +0530

    Added UI elements and their corresponding connections.

--- Whereami/WhereamiViewController.h
+++ Whereami/WhereamiViewController.h
@@ -1,9 +1,14 @@
 #import <UIKit/UIKit.h>
-#import "CoreLocation/CoreLocation.h"
+#import <CoreLocation/CoreLocation.h>
+#import <MapKit/MapKit.h>
 
 @interface WhereamiViewController : UIViewController <CLLocationManagerDelegate>
 {
     CLLocationManager *locationManager;
+    
+    IBOutlet MKMapView *worldView;
+    IBOutlet UIActivityIndicatorView *activityIndicator;
+    IBOutlet UITextField *locationTitleField;
 }
 
 @end
